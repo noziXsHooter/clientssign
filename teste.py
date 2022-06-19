48a28555c297e58be9ff5f4c1fda7dac251e2895
noziX <94942615+noziXsHooter@users.noreply.github.com>
2022-06-19 18:42:57 -0300

    comprasdb

--- teste.py
+++ teste.py
@@ -1,7 +1,25 @@
 
-from cadastraclientes import CadastraCliente, Dado
+#from cadastraclientes import CadastraCliente, Dado
 from encontracliente import*
+from datetime import datetime
 import json
+
+
+#dataagora = datetime.now()
+
+#print(dataagora)
+#print(dataagora.strftime("%d/%m/%Y %H:%M:%S"))
+
+class TimeDB:
+    def envia_tempo(self):
+        self.dataagora = datetime.now()
+        print(self.dataagora.strftime("%d/%m/%Y %H:%M:%S"))
+
+
+if __name__ == '__main__':
+    exectime = TimeDB()
+    exectime.envia_tempo()
+
 
 #teste = CadastraCliente()
 #dcompra = Dado(nome='Marcelo', endereco='Rua', telefone='2185763200', nota='150', valor='5000')
@@ -9,7 +27,7 @@ import json
 #teste.transforma_dict()
 #teste.salva_na_db()
 
-busca = EncontraCliente()
-dbusca = Object('Junior')
-busca.insere_objeto(dbusca)
-busca.busca_objeto()
+#busca = EncontraCliente()
+#dbusca = Object('Junior')
+#busca.insere_objeto(dbusca)
+#busca.busca_objeto()
